0b5ce43b27991efb53c5d411c99f56db5be45405
EmilianoAprile <emilianoaprile77@icloud.com>
2023-11-02 15:50:37 +0100

    ADDED - pushed email in empty array

--- js/script.js
+++ js/script.js
@@ -1,20 +1,46 @@
-console.log('Vue email')
+console.log('Vue email');
 
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            emailList: []
+            emailList: [],
         }
     },
     methods: {
 
+        fetchEmail() {
+
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+
+                    const email = res.data.response;
+
+                    this.emailList.push(email);
+
+                    console.log(this.emailList);
+                    console.log(res.data);
+                })
+        }
+
     },
+    created() {
+		this.fetchEmail()
+	},
 }).mount('#app')
 
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then((res) => {
-        console.log(res.data)
-    })
+
+// axios
+//     .get('https://flynn.boolean.careers/exercises/api/random/mail')
+//     .then((res) => {
+
+//         const email = res.data.response;
+
+//         this.emailList.push(email);
+
+//         console.log(this.emailList);
+//         console.log(res.data);
+//     })

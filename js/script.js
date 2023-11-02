2d88ddf423bccd7c9b435ec7f053d54eac5dd46e
EmilianoAprile <emilianoaprile77@icloud.com>
2023-11-02 15:18:53 +0100

    first commit: basic structure

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('Vue email')
+
+const { createApp} = Vue;
+
+createApp ({
+    data() {
+        return {
+            message: 'hello'
+        }
+    },
+    methods: {
+
+    },
+}).mount('#app')
